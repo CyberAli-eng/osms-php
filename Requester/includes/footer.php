a2f34960c452222533633c814c02b9fad9fb79a5
Ali Khusroo Bin Sabir <alisabir97570@gmail.com>
2025-06-04 16:28:20 +0530

    Initial commit: Upload OSMS PHP project

---
+++ Requester/includes/footer.php
@@ -0,0 +1,21 @@
+  </div> <!-- End Row -->
+ </div> <!-- End Container -->
+
+
+  <!-- Start Footer -->
+ <footer class="container-fluid bg-dark mt-5 text-white">
+  <div class="container">
+   <div class="row py-3">
+    <div class="col-md-6 text-right"> <!-- Start 2nd Column -->
+      <p>&copy; 2025 CyberAli-eng and Ali Khusroo Bin Sabir</p>
+    </div> <!-- End 2nd Column -->
+   </div>
+  </div>
+ </footer>
+ <!-- JavaScript -->
+ <script src="../js/jquery.min.js"></script>
+ <script src="../js/popper.min.js"></script>
+ <script src="../js/bootstrap.min.js"></script>
+ <script src="../js/all.min.js"></script>
+</body>
+</html>
